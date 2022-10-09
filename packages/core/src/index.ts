02b8e6dd5d206341341b84ea4e03dd4235f15d05
Clément Creusat <clement.creusat@gmail.com>
2022-10-09 17:47:04 +0200

    feat(core): add combobox component

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 // Components
 export { default as Button } from "./Button/Button";
 export { default as Accordion } from "./Accordion/Accordion";
+export { default as Combobox } from "./Combobox/Combobox";
