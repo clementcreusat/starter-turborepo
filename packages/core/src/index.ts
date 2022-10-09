fd05debc48c77c9290302fd9c39fde7296b9b5f9
Clément Creusat <clement.creusat@gmail.com>
2022-10-09 17:55:03 +0200

    feat: add accordion component

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
 // Components
 export { default as Button } from "./Button/Button";
 export { default as Accordion } from "./Accordion/Accordion";
-export { default as Combobox } from "./Combobox/Combobox";
